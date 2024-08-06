c7b6399ff9027053617f7f913a2d65900bf21dc6
Shareef <shareefsaning1@gmail.com>
2024-08-06 09:35:50 +0700

    test detect by haarcascades_cuda

--- TEST/test.cpp
+++ TEST/test.cpp
@@ -1,5 +1,5 @@
 /*
-cd {$workingspace}> g++ -o TEST/test TEST/test.cpp -I"C:/msys64/ucrt64/include/opencv4" -L"C:/msys64/ucrt64/lib" -lopencv_core -lopencv_imgproc -lopencv_highgui -lopencv_videoio
+g++ -o TEST/test TEST/test.cpp -I"C:/msys64/ucrt64/include/opencv4" -L"C:/msys64/ucrt64/lib" -lopencv_core -lopencv_imgproc -lopencv_highgui -lopencv_videoio -lopencv_objdetect
 */
 
 #include <opencv2/opencv.hpp>
@@ -8,33 +8,43 @@ cd {$workingspace}> g++ -o TEST/test TEST/test.cpp -I"C:/msys64/ucrt64/include/o
 using namespace std;
 
 int main(){
-    cv::VideoCapture cap(0);
-    
+    cv::Mat frame;  // store image data pixel value
+    cv::VideoCapture cap(0);    // Capture device 0 
+    cv::CascadeClassifier detectionClassifier;  // Load Classifier
+
+    if(!detectionClassifier.load("../data/haarcascades_cuda/haarcascade_frontalface_default.xml")){
+        cout << "XML file not loaded" << endl;
+        exit(0);
+    }
+
     if (!cap.isOpened()) {
         cerr << "Error opening video stream" << endl;
         return -1;
     }
     else {
-    cout << "Camera opened successfully." << endl;
+        cout << "Camera opened successfully." << endl;
     }
 
-    cv::Mat frame;
-    while(1){
-        if(!cap.read(frame)){
-            cout << "Video camera is disconnected" << endl;
-            cin.get();
-            break;
-        };
-
+    while(cap.read(frame)){
         if (frame.empty()) {
-            cerr << "Error: Empty frame captured." << std::endl;
+            cerr << "Captured an empty frame" << endl;
             break;
+        }
+
+        vector<cv::Rect> _detection;
+        cv::Mat outputframe;
+        cv::cvtColor(frame, outputframe, cv::COLOR_BGR2GRAY);   // Convert the frame to grayscale
+        detectionClassifier.detectMultiScale(outputframe, _detection);  // Detect objects in the frame
+
+        // Draw a detections on the frame
+        for (const auto& value : _detection) {
+            cv::rectangle(frame, value, cv::Scalar(255, 0, 0), 2, 8, 0);
         }
 
         // Display the frame
         cv::imshow("Video Capture TESTETS", frame);
         
-        if (cv::waitKey(30) == 'q' || cv::waitKey(30) == 27) { // 27 is the ASCII code for ESC key
+        if (cv::waitKey(10) == 'q' || cv::waitKey(10) == 27) { // 27 is the ASCII code for ESC key
             break;
         }
     }
